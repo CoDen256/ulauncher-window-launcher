97951258ab899019870d0a47898c7336fc6c17ed
Denys Chernyshov <den.blackshov@gmail.com>
2021-04-25 21:42:36 +0200

    IT works omg

--- main.py
+++ main.py
@@ -4,26 +4,39 @@ from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.search.apps.AppDb import AppDb
+import logging
 
+from ulauncher.utils.desktop.reader import find_apps_cached
 
+logger = logging.getLogger(__name__)
 class DemoExtension(Extension):
 
     def __init__(self):
         super(DemoExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
+        for app in find_apps_cached():
+            AppDb.get_instance().put_app(app)
+        logger.info(list(find_apps_cached()))
+
 
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
+        query = event.get_argument()
+        logger.info(query)
+        logger.info(list(AppDb.get_instance().get_records()))
+        result_list = AppDb.get_instance().find(query)
+        logger.info(result_list)
         items = []
-        for i in range(5):
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='Item %s' % i,
-                                             description='Item description %s' % i,
-                                             on_enter=HideWindowAction()))
-
+        for app_result_item in result_list:
+            items.append(ExtensionResultItem(icon='images/icon.png', # not PixBuf, okay?
+                                                name=app_result_item.get_name(),
+                                                description=app_result_item.get_description(None),
+                                                on_enter=HideWindowAction()))
         return RenderResultListAction(items)
 
 if __name__ == '__main__':
+    logger.info("HELO ITS ME MARIO")
     DemoExtension().run()
